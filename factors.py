40c5933cee46e01befb9fb6fde95b4021fb54a72
yousrahibasara <yousrahoumma92@gmail.com>
2023-10-28 09:32:58 -0700

    factors.py

--- factors.py
+++ factors.py
@@ -1,33 +1,94 @@
-#!/usr/bin/python3
-"""Module that factorize as many numbers as possible
- into a product of two smaller numbers."""
-from sys import argv
+import random
+import math
+
+#select 2 large prime numbers
+def generate_p_and_q():
+	
+	#Calculating 1 to 100 prime numbers 
+
+	numbs = [i for i in range(2,101)]
+
+	for n in range(2,101):
+		for i in range(2,math.ceil(n/2)+1):
+			if n % i == 0:
+				numbs.remove(n)
+				break
+			else:
+				continue
+
+	#Selecting any 2 prime numbers randomly
+
+	p = random.choice(numbs)
+	numbs.remove(p)
+	q = random.choice(numbs)
+
+	return p, q
+
+p,q =  generate_p_and_q()
+
+print(f'[+] p = {p} and q = {q}')
+
+n = p * q
+
+phi = (p - 1) * (q - 1)
 
 
-def factorize(value):
-    """"print a simple descomposition of an integer > 1"""
-    i = 2
+print(f'[+] n = {n} and euler totient = {phi}')
 
-    if value < 2:
-        return
-    print()
-    print(value, "<- value-bef")
-    while value % i:
-        i += 1
-    print("{:.0f}={:.0f}*{:.0f}".format(value, value / i, i))
-    print(value, "<- value-aft")
-    print()
 
-if len(argv) != 2:
-    exit()
+#Calculating e -> gcd(e,phi) = 1 and 1 < e <phi.
+def generate_e(phi):
+	possible_e_values = []
 
-try:
-    with open(argv[1]) as file:
-        line = file.readline()
+	for i in range(2,phi):
+		if math.gcd(i,phi) == 1: 
+			e=i
+			possible_e_values.append(e)
 
-        while line != "":
-            value = int(line.split('\n')[0])
-            factorize(value)
-            line = file.readline()
-except:
-    pass
+	# print(possible_e_values)
+
+	return random.choice(possible_e_values)
+
+e = generate_e(phi)
+
+print(f'[+] e = {e}')
+
+def generate_d(e,phi):
+
+	# d_list = [] 
+
+	for i in range(2,phi):
+
+		if (i*e) % phi == 1: #  ed mod(phi) = 1
+			d = i # As every unique public key have only one unique private key.
+			# d_list.append(d)
+			break
+
+	# print(d_list)
+	return d
+
+d = generate_d(e,phi)
+
+print(f'[+] d = {d}')
+
+# Message should be less than n (msg < n)
+
+msg = random.randint(1,n)
+
+print(f'[+] msg : {msg}')
+
+def encrypt(msg,e,n): #(msg^e) mod n
+	c = pow(msg,e,n)
+	return c
+
+e_msg = encrypt(msg,e,n)
+
+print(f'[+] Encrypted msg : {e_msg}')
+
+def decrypt(msg,d,n): #(msg^d) mod n
+	p = pow(msg,d,n)
+	return p
+
+d_msg = decrypt(e_msg,d,n)
+
+print(f'[+] Decrypted msg : {d_msg}')
